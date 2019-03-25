d2ebea63fea1f1b239e43693cddc36c4c3e176ce
takahashi たかはし こうじ <koji-takahashi@gmo.jp>
2019-03-25 18:34:25 +0900

    [add]ひな形コントローラー

--- MyApp/MyApp/App_Start/BundleConfig.cs
+++ MyApp/MyApp/App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace MyApp
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/style.css"));
         }
     }
 }
